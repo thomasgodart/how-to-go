e2d37b85f8bbb8e61e0d3324e66128c098d42f40
Thomas <thomas.godart@gadz.org>
2020-10-30 20:22:09 +0100

    install instructions

--- 2-srv/2-system/0-notify/install.sh
+++ 2-srv/2-system/0-notify/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+go get github.com/coreos/go-systemd/daemon
+
 cd /etc/systemd/system/
 
 sudo ln -sf /var/www/go/how-to-go/2-srv/2-system/0-notify/system.service how-srv-system0.service
--- 2-srv/2-system/1-socket/install.sh
+++ 2-srv/2-system/1-socket/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+go get github.com/coreos/go-systemd/activation
+go get github.com/coreos/go-systemd/daemon
+
 cd /etc/systemd/system/
 
 sudo ln -sf /var/www/go/how-to-go/2-srv/2-system/1-socket/system.service how-srv-system1.service
